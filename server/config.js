8515b7b80128e26156115177e688210ed8202cf7
Tejmq <75015175+Tejmq@users.noreply.github.com>
2026-04-15 16:12:00 +0300

    test

--- server/config.js
+++ server/config.js
@@ -33,12 +33,11 @@ module.exports = {
 
             properties: { // This overrides settings in the config.js file, assuming the selected gamemode doesn't also override it.
            //     teams: 3,
-                bot_cap: 20
+                bot_cap: 20,
                 daily_tank: {
                     tank: 'whirlwind',
                     tier: 3,
-                    ads: false,
-
+                    ads: false
                 },
                 
             }
